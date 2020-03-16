357107118f680f7008c8092dc10887cec1b57842
WKD622 <kuba.ziarko@gmail.com>
2020-03-16 17:21:42 +0100

    commit

--- sequential.c
+++ sequential.c
@@ -1,62 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <time.h>
- 
- 
-long double PI = 3.14159265358979323L;
- 
-double calc_pi(long long int number_of_points) {
-    long long int i, circle_points = 0;
-    double rand_x, rand_y, origin_dist;
- 
-    for (i = 0; i < number_of_points; i++) {
-        rand_x = (double) (rand() / ((double) RAND_MAX));
-        rand_y = (double) (rand() / ((double) RAND_MAX));
- 
-        origin_dist = rand_x * rand_x + rand_y * rand_y;
- 
-        if (origin_dist <= 1)
-            circle_points++;
-    }
- 
-    return (double) (4 * circle_points) / (double) number_of_points;
+#include <math.h>
+
+long double R_PI = 3.14159265358979323L;
+
+
+double rand_value(double start, double end)
+{
+    return (double)rand() / RAND_MAX * (end - start) + start;
 }
- 
-int main(int argc, char** argv) {
-    long long int number_of_points;
-    int calc_repeat;
- 
-    if (argc != 3) {
-        return 1;
+
+double x_2(double x)
+{
+    return pow(x, 2.0);
+}
+
+double monte_carlo(unsigned long long int number_of_shoots, double x_start, double x_end, double y_start, double y_end)
+{
+    int success = 0;
+    int loss = 0;
+    unsigned long long int i = 0;
+    for (i = 0; i < number_of_shoots; i++)
+    {
+        double x = rand_value(x_start, x_end);
+        double y = rand_value(y_start, y_end);
+
+        double x_pow_value = x_2(x);
+        double y_pow_value = x_2(y);
+        double sum = x_pow_value + y_pow_value;
+        if (sum <= 1)
+            success++;
+        else
+            loss++;
     }
-    number_of_points = strtoll(argv[1], NULL, 0);
-    calc_repeat = atoi(argv[2]);
- 
-    srand(time(NULL));
- 
-    double avg = 0.0, std_dev = 0.0, avg_t = 0.0;
-    double* pi_results = (double*) malloc(sizeof(double) * calc_repeat);
-    int i;
-     for (i = 0; i < calc_repeat; i++) {
-        clock_t t = clock();
-        pi_results[i] = calc_pi(number_of_points);
-        avg_t += clock() - t;
-        avg += pi_results[i];        
-    }
-    avg /= calc_repeat;
-    avg_t = avg_t / (CLOCKS_PER_SEC * calc_repeat);
-       
-    for (i = 0; i < calc_repeat; i++) {
-        std_dev += (pi_results[i] - avg) * (pi_results[i] - avg);
-    }
-    std_dev = sqrt(std_dev / calc_repeat);
-   
-    printf("=== RESULT FOR %d POINTS ===\n", number_of_points);
-    printf("AVG: %f\n", avg);
-    printf("DEV: %f\n", std_dev);
-    printf("DIF: %lf\n", fabs(PI - avg));
-    printf("TIME: %f\n\n", avg_t);
- 
+    return ((double)success / ((double)loss + (double)success)) * (x_end - x_start) * (y_end - y_start);
+}
+
+int main(int argc, char** argv)
+{
+    unsigned long long int number_of_shoots = strtoll(argv[1], NULL, 0);
+    double area_size = monte_carlo(number_of_shoots, 0.0, 1.0, 0.0, 1.0);
+    double pi = 4 * area_size;
+    printf("PI: %Lf\n", pi);
+    printf("DIF: %.14f\n", fabs(R_PI - pi));
+    printf("TIME: %f\n\n", t);
     return 0;
 }
